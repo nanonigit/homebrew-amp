8ca397c29bed836c6aaab7f25919bb11a7cf5f07
nanonigit <nanoni@gmail.com>
2026-08-17 11:10:30 +0700

    Update amp and vibe to v0.21.0

--- Casks/amp.rb
+++ Casks/amp.rb
@@ -1,6 +1,6 @@
 cask "amp" do
-  version "0.20.0"
-  sha256 "37cb341f950e9a1d2f690a763cb160c5fceea6a86ab63ffbfefc820dafe06642"
+  version "0.21.0"
+  sha256 "5467ea44320ff14f834764b2c7e074fc08019d9099f837f1ea97a09154af7b03"
 
   url "https://github.com/nanonigit/Vibe/releases/download/v#{version}/amp-v#{version}-macos-arm64.zip"
   name "amp"
--- Casks/vibe.rb
+++ Casks/vibe.rb
@@ -1,6 +1,6 @@
 cask "vibe" do
-  version "0.20.0"
-  sha256 "37cb341f950e9a1d2f690a763cb160c5fceea6a86ab63ffbfefc820dafe06642"
+  version "0.21.0"
+  sha256 "5467ea44320ff14f834764b2c7e074fc08019d9099f837f1ea97a09154af7b03"
 
   url "https://github.com/nanonigit/Vibe/releases/download/v#{version}/amp-v#{version}-macos-arm64.zip"
   name "amp"
@@ -18,7 +18,7 @@ cask "vibe" do
   app "amp.app"
 
   caveats <<~EOS
-    amp is ad-hoc signed and is not notarized by Apple. If macOS blocks the
+    amp (formerly Vibe) is ad-hoc signed and is not notarized by Apple. If macOS blocks the
     first launch, open /Applications in Finder, Control-click amp, choose
     Open, and review the warning before continuing.
   EOS
