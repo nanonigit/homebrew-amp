ef09ea37626a1edaa6da22109e79d7d5ef237ba2
nanonigit <nanoni@gmail.com>
2026-08-17 11:15:14 +0700

    Update amp cask sha256 with borderless app icon

--- Casks/amp.rb
+++ Casks/amp.rb
@@ -1,6 +1,6 @@
 cask "amp" do
   version "0.21.0"
-  sha256 "5467ea44320ff14f834764b2c7e074fc08019d9099f837f1ea97a09154af7b03"
+  sha256 "dafb7bcf133c1099c52dea1dd043b4a2973c83e34bbde32ddd04da85c85bc2bd"
 
   url "https://github.com/nanonigit/amp/releases/download/v#{version}/amp-v#{version}-macos-arm64.zip"
   name "amp"
